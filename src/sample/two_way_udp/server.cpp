89769ce6b4bddc6a4d7dbc190800b5a9fa7232bc
masa25hw <masa25hw@gmail.com>
2023-10-09 01:15:24 +0900

    feat: Add two-way UDP communication sample codes

---
+++ src/sample/two_way_udp/server.cpp
@@ -0,0 +1,31 @@
+#include "user_config.hpp"
+#include "socket.hpp"
+
+#include <iostream>
+
+#include "spdlog/spdlog.h"
+
+
+int main() {
+    Socket socket;
+    uint16_t port = NetworkConfig::SERVER_PORT;
+    if (!socket.Bind(port)) {
+        return 1;
+    }
+
+    while (true) {
+        std::string request, client_ip;
+        uint16_t client_port;
+        if (socket.Receive(request, client_ip, client_port)) {
+            spdlog::info("Received request from {0}:{1}: {2}", client_ip, client_port, request);
+
+            // Process the request (e.g., send a response)
+            std::string response = "Hello, Client!";
+            if (!socket.Send(response, client_ip, client_port)) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
